d9aa72c36e24a62fa46a3f6a5ac8cbb342c2c827
alina <alinkash2811@gmail.com>
2025-05-05 12:33:15 +0200

    add faq

---
+++ src/js/faq.js
@@ -0,0 +1,17 @@
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+
+// new Accordion('[data-accordion]');
+
+// export function AccordionInIt() {
+//   const trigger = document.querySelector('[data-accordion]');
+
+//   trigger.addEventListener('click', () => {
+//     const item = evt.target.closest('li');
+//     if (item === null) {
+//       return;
+//     }
+
+//     panel.dataset.panel = 'open';
+//   });
+// }
