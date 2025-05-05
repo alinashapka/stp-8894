4ea3f54f9d01f494595b83a4e048dc0f08396c22
alina <alinkash2811@gmail.com>
2025-05-05 16:10:39 +0200

    update faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,17 +1,29 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-// new Accordion('[data-accordion]');
+new Accordion('[data-accordion]');
 
-// export function AccordionInIt() {
-//   const trigger = document.querySelector('[data-accordion]');
+export function AccordionInIt() {
+  const triggers = document.querySelectorAll('[data-accordion-trigger]');
 
-//   trigger.addEventListener('click', () => {
-//     const item = evt.target.closest('li');
-//     if (item === null) {
-//       return;
-//     }
+  triggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+      const panel = trigger.lastElementChild;
+      const questionText = trigger.querySelector('[data-text]');
+      const icon = trigger.querySelector('[data-icon]');
+      const button = trigger.querySelector('[data-button]');
 
-//     panel.dataset.panel = 'open';
-//   });
-// }
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+        questionText.classList.remove('active');
+        icon.setAttribute('href', '/img/sprite.svg#icon-plus');
+        button.style.marginTop = '0';
+      } else {
+        panel.style.display = 'block';
+        questionText.classList.add('active');
+        icon.setAttribute('href', '/img/sprite.svg#icon-minus');
+        button.style.marginTop = '55.5px';
+      }
+    });
+  });
+}
